d2cd079cc8881f9dacb0a99708b72790c3e04828
TgSkyline <tigomes.santos@gmail.com>
2025-05-09 05:56:49 -0300

    Variáveis Globais e NonLocal em Funções

--- pratica.py
+++ pratica.py
@@ -64,9 +64,32 @@ Exercício 5
 # conjunto = {1, 2, 3, 4, 5}
 # help(conjunto)
 
-from collections import Counter
+# from collections import Counter
 
-lista = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,1,1,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10)
-res = Counter(lista)
-print(res)
-print(res.get(1))
+# lista = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,1,1,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10)
+# res = Counter(lista)
+# print(res)
+# print(res.get(1))
+
+# total = 0
+
+# def soma():
+#     global total
+#     total += 1
+#     return total
+
+# print(soma())
+# print(soma())
+# print(soma())
+
+def fora():
+    contador = 0
+    def dentro():
+        nonlocal contador
+        contador += 1
+        return contador
+    return dentro()
+
+print(fora())
+print(fora())
+print(fora())
