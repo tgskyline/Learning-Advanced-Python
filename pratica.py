fc6ee448e327529012a99b8df27070180286af92
TgSkyline <tigomes.santos@gmail.com>
2025-05-27 05:10:18 -0300

    Dictionary Comprehensions

--- pratica.py
+++ pratica.py
@@ -96,5 +96,25 @@ Exercício 5
 # print(fora())
 # print(fora())
 
-amigos = ['maria', 'joão','carlos']
-print([amigo[0].upper()+amigo[1:10000] for amigo in amigos ])
+# amigos = ['maria', 'joão','carlos']
+# print([amigo[0].upper()+amigo[1:10000] for amigo in amigos ])
+
+# numeros={'a':1,'b':2,'c':3,'d':4,'e':5}
+
+# quadrado= {chave: valor **2 for chave, valor in numeros.items()}
+
+# print(quadrado)
+
+
+# chaves= 'abcde'
+# numeros= [1,2,3,4,5]
+
+# mistura= {chaves[i]: numeros[i] for i in range(0, len(chaves))}
+
+# print(mistura)
+
+numeros = [1, 2, 3, 4, 5]
+
+res ={num:('par' if num % 2 == 0 else 'impar') for num in numeros}
+
+print(res)
